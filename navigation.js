50e4f92dffb5cb653edd01c78cc6db7d2ae88794
enikop <90562775+enikop@users.noreply.github.com>
2023-12-15 15:39:54 +0100

    Homepage ready
    CSS needs refactoring

---
+++ navigation.js
@@ -0,0 +1,21 @@
+function clickHome(){
+    location.href = "index.html"
+}
+
+function clickShop(){
+    console.log("futok")
+    location.href = "shop.html"
+}
+
+function clickSeance(){
+    location.href = "shop.html"
+}
+
+function clickDivination(){
+    location.href = "shop.html"
+}
+
+function clickColleagues(){
+    location.href = "shop.html"
+
+}
